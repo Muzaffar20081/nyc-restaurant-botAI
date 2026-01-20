d1cd1f0822f22a98c06a10c5fb6e49f1e95d9922
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2026-01-20 13:34:23 -0500

    Delete menu directory

--- menu/burgers.py
+++
@@ -1,6 +0,0 @@
-# menu/burgers.py
-burgers = [
-    {"id": "burger1", "name": "🍔 Классический", "price": 350},
-    {"id": "burger2", "name": "🍔 Чизбургер", "price": 400},
-    {"id": "burger3", "name": "🍔 Бекон", "price": 450},
-]
