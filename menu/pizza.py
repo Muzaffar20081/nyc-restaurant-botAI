2a09e1f2799343d5b717a4350adf9543bebadc9f
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2026-01-20 13:25:28 -0500

    Update pizza.py

---
+++ menu/pizza.py
@@ -0,0 +1,6 @@
+# menu/pizza.py
+pizza = [
+    {"id": "pizza1", "name": "🍕 Маргарита", "price": 550},
+    {"id": "pizza2", "name": "🍕 Пепперони", "price": 600},
+    {"id": "pizza3", "name": "🍕 4 сыра", "price": 650},
+]
